672579e8a92093f1b1f78e08950d4d0755223e91
085_Teuzin <158859898+085Teuzin@users.noreply.github.com>
2026-04-23 17:37:46 -0300

    Add files via upload

---
+++ Fases.js
@@ -0,0 +1,13 @@
+export const FASES = [
+    {
+        id: 1,
+        nome: "O Início",
+        plataformas: [
+            { x: 0, y: 0, z: 0, w: 5, d: 5, cor: 0x444444 },   // Plataforma inicial
+            { x: 0, y: 1, z: -6, w: 3, d: 3, cor: 0x00ff00 },  // Pulo 1
+            { x: 4, y: 2, z: -10, w: 2, d: 2, cor: 0x00ff00 }, // Pulo 2
+            { x: 0, y: 3, z: -15, w: 3, d: 3, cor: 0x00ff00 }, // Pulo 3
+            { x: 0, y: 4, z: -22, w: 6, d: 6, cor: 0xffd700 }  // Chegada (Dourada)
+        ]
+    }
+];
