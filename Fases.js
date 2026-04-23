201bcf8f48b9eba66d52afec609446ef29d00da1
085_Teuzin <158859898+085Teuzin@users.noreply.github.com>
2026-04-23 17:43:30 -0300

    Atualizar o Fases.js

--- Fases.js
+++ Fases.js
@@ -1,13 +1,34 @@
 export const FASES = [
     {
-        id: 1,
-        nome: "O Início",
+        nome: "Nível 1",
         plataformas: [
-            { x: 0, y: 0, z: 0, w: 5, d: 5, cor: 0x444444 },   // Plataforma inicial
-            { x: 0, y: 1, z: -6, w: 3, d: 3, cor: 0x00ff00 },  // Pulo 1
-            { x: 4, y: 2, z: -10, w: 2, d: 2, cor: 0x00ff00 }, // Pulo 2
-            { x: 0, y: 3, z: -15, w: 3, d: 3, cor: 0x00ff00 }, // Pulo 3
-            { x: 0, y: 4, z: -22, w: 6, d: 6, cor: 0xffd700 }  // Chegada (Dourada)
+            { x: 0, y: 0, z: 0, w: 6, d: 6, cor: 0x555555 },
+            { x: 0, y: 0.5, z: -8, w: 3, d: 3, cor: 0x00ff88 },
+            { x: 5, y: 1, z: -13, w: 3, d: 3, cor: 0x00ff88 },
+            { x: 0, y: 2, z: -18, w: 3, d: 3, cor: 0x00ff88 },
+            { x: 0, y: 3, z: -28, w: 6, d: 6, cor: 0xffd700 } 
+        ]
+    },
+    {
+        nome: "Nível 2",
+        plataformas: [
+            { x: 0, y: 0, z: 0, w: 5, d: 5, cor: 0x555555 },
+            { x: -4, y: 1, z: -6, w: 2, d: 2, cor: 0x00ccff },
+            { x: 0, y: 2, z: -12, w: 2, d: 2, cor: 0x00ccff },
+            { x: 4, y: 3, z: -18, w: 2, d: 2, cor: 0x00ccff },
+            { x: 0, y: 4, z: -24, w: 2, d: 2, cor: 0x00ccff },
+            { x: 0, y: 5, z: -32, w: 6, d: 6, cor: 0xffd700 }
+        ]
+    },
+    {
+        nome: "Nível 3",
+        plataformas: [
+            { x: 0, y: 0, z: 0, w: 4, d: 4, cor: 0x555555 },
+            { x: 0, y: 3, z: -7, w: 2, d: 2, cor: 0xff5500 },
+            { x: 6, y: 6, z: -7, w: 2, d: 2, cor: 0xff5500 },
+            { x: 6, y: 9, z: -14, w: 2, d: 2, cor: 0xff5500 },
+            { x: 0, y: 12, z: -14, w: 2, d: 2, cor: 0xff5500 },
+            { x: 0, y: 13, z: -22, w: 8, d: 8, cor: 0xffd700 }
         ]
     }
 ];
